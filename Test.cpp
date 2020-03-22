a994ede00d0c4c0b87f5bdd5bda5c1deebd970de
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-03-22 20:15:07 +0200

    add verity of tests

--- Test.cpp
+++ Test.cpp
@@ -2,10 +2,42 @@
 #include "doctest.h"
 #include "PhoneticFinder.hpp"
 #include <string>
-using namespace std;
+
+/*
+Letters that can be switchd:
+	v, w
+	b, f, p
+	g, j
+	c, k, q
+	s, z
+	d, t
+	o, u
+	i, y
+	
+And all letter can be capital and small.
+*/
 
 TEST_CASE("famous quotes") {
-    string text1 = "The order of the phoenix";
+	string text0 = "Very good famous quotes"; 
+	string parmotaitionOf_Very[5] = { "Wery","wERI","Veri","veri","veRY"};
+	for (int i = 0; i < 5; i++)
+		CHECK(find(text2, parmotaitionOf_Very[i]) == string("Very"));
+    
+	string parmotaitionOf_good[5] = { "Gout","gOUT","JooD","juud","gOOd"};
+	for (int i = 0; i < 5; i++)
+		CHECK(find(text2, parmotaitionOf_good[i]) == string("good"));
+    
+	string parmotaitionOf_famous[6] = { "bamuus","BamooS","FAMOUs","famouz",	
+										"PamuuZ","pamuoz"};
+	for (int i = 0; i < 6; i++)
+		CHECK(find(text2, parmotaitionOf_famous[i]) == string("famous"));
+    
+	string parmotaitionOf_quotes[10] = { "qootes","COODEZ","kuuTeS","CUotez","cuotez",
+										 "Koodes","Quotez","cuotes","qoutes","quotes"};
+	for (int i = 0; i < 10; i++)
+		CHECK(find(text2, parmotaitionOf_quotes[i]) == string("quotes"));
+	
+    string text1 = "The order of the phoenix"; 
 	string parmotaitionOf_The[5] = { "the", "THE", "thE", "dhe", "Dhe"};
 	for (int i = 0; i < 5; i++)
 		CHECK(find(text1, parmotaitionOf_The[i]) == string("The"));
@@ -14,16 +46,19 @@ TEST_CASE("famous quotes") {
 	for (int i = 0; i < 5; i++)
 		CHECK(find(text1, parmotaitionOf_order[i]) == string("order"));
     
-	string parmotaitionOf_of[10] = { "of", "OF", "uF", "Ub", "UB", "ob", "OB", "op", "OP", "up"};
+	string parmotaitionOf_of[10] = { "of", "OF", "uF", "Ub", "UB",
+									 "ob", "OB", "op", "OP", "up"};
 	for (int i = 0; i < 10; i++)
 		CHECK(find(text1, parmotaitionOf_order[i]) == string("order"));
     
-	string parmotaitionOf_phoenix[5] = { "phoenix", "PhoenYx", "Fhuenix", "bHOENIX", "bhuenyx"};
+	string parmotaitionOf_phoenix[5] = { "phoenix", "PhoenYx", "Fhuenix",
+										 "bHOENIX", "bhuenyx"};
 	for (int i = 0; i < 5; i++)
 		CHECK(find(text1, parmotaitionOf_phoenix[i]) == string("phoenix"));
     
 	string text2 = "To be or not to be";
-	string parmotaitionOf_To[10] = { "To", "TO", "Tu","TU", "to", "tO", "tu","tU", "Do","dO"};
+	string parmotaitionOf_To[10] = { "To", "TO", "Tu","TU", "to",
+									 "tO", "tu","tU", "Do","dO"};
 	for (int i = 0; i < 10; i++)
 		CHECK(find(text2, parmotaitionOf_To[i]) == string("To"));
     
@@ -35,8 +70,8 @@ TEST_CASE("famous quotes") {
 	for (int i = 0; i < 5; i++)
 		CHECK(find(text2, parmotaitionOf_or[i]) == string("or"));
     
-	string parmotaitionOf_not[5] = { "or", "OR", "ur", "Ur", "uR"};
+	string parmotaitionOf_not[5] = { "Not", "nUt", "nud", "NUT", "nuD"};
 	for (int i = 0; i < 5; i++)
-		CHECK(find(text2, parmotaitionOf_or[i]) == string("or"));
+		CHECK(find(text2, parmotaitionOf_or[i]) == string("not"));
     
 }
